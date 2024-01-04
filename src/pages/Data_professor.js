bfc3ad2a6f485d7e8d23d556e009050125d0614a
PhatPHATnaja <phatphantomblood@gmail.com>
2024-01-04 20:31:47 +0700

    ADDall

--- src/pages/Data_professor.js
+++ src/pages/Data_professor.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
+import { DataView } from 'primereact/dataview';
 
 function Data_professor() {
   const [professorData, setProfessorData] = useState([]);
@@ -11,16 +11,43 @@ function Data_professor() {
 
   const itemTemplate = (professor) => (
     <div className="p-col-12 professor-item">
-      <div><strong>รหัสพนักงาน:</strong> {professor.professor_user_id}</div>
-      <div><strong>ชื่อ:</strong> {professor.professor_first_name}</div>
-      <div><strong>นามสกุล:</strong> {professor.professor_last_name}</div>
-      <div><strong>ตำแหน่งย่อ:</strong> {professor.professor_position}</div>
-      <div><strong>สังกัด:</strong> {professor.professor_faculty}</div>
+      <div className='data_professor'>
+        <div>
+          <strong><h1>ชื่อ - นามสกุล:</h1></strong>
+          <h1>{professor.professor_first_name} {professor.professor_last_name}</h1>
+        </div>
+        <br/>
+        <div>
+          <strong><h1>คณะ:</h1></strong>
+          <h1>{professor.professor_faculty}</h1>
+        </div>
+        <br/>
+        <div>
+          <strong><h1>สาขา :</h1></strong>
+          <h1>{professor.professor_branch}</h1>
+        </div>
+        <br/>
+        <div>
+          <strong><h1>ตำแหน่งย่อ:</h1></strong>
+          <h1>{professor.professor_position}</h1>
+        </div>
+        <br/>
+        <div>
+          <strong><h1>คุณวุฒิ :</h1></strong>
+          <h1>{professor.professor_qualification}</h1>
+        </div>
+        <br/>
+        <div>
+          <strong><h1>เพศ :</h1></strong>
+          <h1>{professor.professor_gender}</h1>
+        </div>
+        <br/>
+      </div>
     </div>
   );
 
   return (
-    <div className='data_student' style={{ width: '100%', marginLeft: '100px', marginTop: '50px' }}>
+    <div style={{ width: '100%', marginLeft: '50px', marginTop: '50px' }}>
       <div className='grid'>
         <div className='col'>
           <DataView value={professorData} itemTemplate={itemTemplate} layout="list" />
