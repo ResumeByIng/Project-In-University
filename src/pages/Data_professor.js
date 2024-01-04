e4235d100f2c8b6b44057acacf988496c3b869d4
Taksinx <zxcasd1175@hotmail.com>
2024-01-04 19:13:02 +0700

    update all fk bug

--- src/pages/Data_professor.js
+++ src/pages/Data_professor.js
@@ -1,42 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { DataView, DataViewLayoutOptions } from 'primereact/dataview';
 
 function Data_professor() {
-  const [professorData, setProfessorData] = useState({});
+  const [professorData, setProfessorData] = useState([]);
 
   useEffect(() => {
-    // ดึงข้อมูลจาก Local Storage โดยใช้ key ที่เก็บข้อมูล
-    const storedProfessorData = localStorage.getItem('userData');
-    
-    if (storedProfessorData) {
-      // แปลงข้อมูลที่ดึงมาจาก Local Storage เป็น Object
-      const parsedProfessorData = JSON.parse(storedProfessorData);
-      
-      // ตั้งค่า state เพื่อให้ React component รีเรนเดอร์
-      setProfessorData(parsedProfessorData);
-    }
+    const userData = JSON.parse(localStorage.getItem('userData'));
+    setProfessorData([userData]);
   }, []);
 
-  // ตรวจสอบว่า professorData มีค่าหรือไม่
-  if (!professorData || !professorData.professor) {
-    return <div className ='data_student' style={{ width: '100%', marginLeft: '100px', marginTop:'50px' }}>Loading...</div>;
-  }
-
-  // เพิ่มตัวกรองเพื่อไม่ให้ email และ password ปรากฏใน JS
-  const { email, password, ...filteredProfessorData } = professorData.professor;
+  const itemTemplate = (professor) => (
+    <div className="p-col-12 professor-item">
+      <div><strong>รหัสพนักงาน:</strong> {professor.professor_user_id}</div>
+      <div><strong>ชื่อ:</strong> {professor.professor_first_name}</div>
+      <div><strong>นามสกุล:</strong> {professor.professor_last_name}</div>
+      <div><strong>ตำแหน่งย่อ:</strong> {professor.professor_position}</div>
+      <div><strong>สังกัด:</strong> {professor.professor_faculty}</div>
+    </div>
+  );
 
   return (
-    <div className ='data_student' style={{ width: '100%', marginLeft: '100px', marginTop:'50px' }}>
-      <h1>ชื่อ-นามสกุล : {filteredProfessorData.first_name} {filteredProfessorData.last_name}</h1>
-      <br />
-      <h1>คณะ : {filteredProfessorData.faculty}</h1>
-      <br />
-      <h1>สาขา : {filteredProfessorData.branch}</h1>
-      <br />
-      <h1>ตำแหน่ง : {filteredProfessorData.position}</h1>
-      <br />
-      <h1>คุณวุฒิ : {filteredProfessorData.qualification}</h1>
-      <br />
-      <h1>เพศ : {filteredProfessorData.gender}</h1>
+    <div className='data_student' style={{ width: '100%', marginLeft: '100px', marginTop: '50px' }}>
+      <div className='grid'>
+        <div className='col'>
+          <DataView value={professorData} itemTemplate={itemTemplate} layout="list" />
+        </div>
+      </div>
     </div>
   );
 }
