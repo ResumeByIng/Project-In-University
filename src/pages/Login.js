9a92fa212b484b0c1961f671db9b4ed2c458ea6e
PhatPHATnaja <phatphantomblood@gmail.com>
2024-01-08 18:22:51 +0700

    ADDNEW

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,19 @@
-import React, { useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useRef } from 'react';
+import { Toast } from 'primereact/toast';
+import { Button } from 'primereact/button';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { FaExclamationCircle } from 'react-icons/fa';
+import { FaCheckCircle } from 'react-icons/fa';
 import './Login.css'; 
 import myImage1 from './ce.png';
+
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [visible, setVisible] = useState(false);
   const navigate = useNavigate();
-
-   // Disable scrolling when the component mounts
-   useEffect(() => {
-    document.body.style.overflow = 'hidden'; // Disable scrolling
-    // Re-enable scrolling when the component unmounts
-    return () => {
-      document.body.style.overflow = 'visible';
-    };
-  }, []);
+  const toast = useRef(null);
 
   const handleLogin = async () => {
     try {
@@ -23,6 +21,7 @@ const Login = () => {
         email: email,
         password: password
       });
+
       const userData = response.data[0];
       const user = response.data[0].role;
 
@@ -32,36 +31,66 @@ const Login = () => {
       console.log('user : ', user);
       console.log('userData : ', userData);
 
-
       switch (user) {
         case 1:
-      case 2:
-      case 3:
-        navigate('/home');
+        case 2:
+        case 3:
+          // Show success Toast when login is successful
+          showSuccessMessage();
+          setTimeout(() => navigate('/home'), 3000);
         break;
-      default:
+        default:
+          // Show error Toast when login fails
+          showErrorMessage();
           console.error('Invalid role:', user);
       }
     } catch (error) {
       console.error('Login failed', error);
+      // Show error Toast when login fails
+      showErrorMessage();
     }
+  };
+
+  const showErrorMessage = () => {
+    toast.current.show({
+      severity: 'error',
+      summary: 'เกิดข้อผิดพลาด',
+      detail: 'ท่านใส่ Email หรือ Password ไม่ถูกต้อง กรุณาลองใหม่อีกครั้ง',
+      life: 3000,
+      position: 'top-center',
+      className: 'custom-toast-error',
+    });
+  };
+
+  const showSuccessMessage = () => {
+    toast.current.show({
+      severity: 'success',
+      summary: 'ล็อคอินสำเร็จ',
+      detail: 'ยินดีต้อนรับ! คุณได้ทำการล็อคอินสำเร็จ',
+      life: 3000,
+      position: 'top-center',
+      className: 'custom-toast-success',
+    });
   };
 
   return (
     <div className="wrapper fadeInDown">
-    <div id="formContent">
-    <div className="fadeIn first ">
+      <div id="formContent">
+        <div className="fadeIn first ">
           <img src={myImage1} alt="รูปภาพ" className="image" />
           <h5>ล็อคอิน</h5>
         </div>
-    <form>
-      <label className="fadeIn second" >Email:</label>
-      <input className="fadeIn second" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <label className="fadeIn third" >Password:</label>
-      <input className="password fadeIn third" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </form>
-      <button className="fadeIn fourth center-button" type="button" onClick={handleLogin}>Login</button>
-    </div>
+        <form>
+          <label className="fadeIn second">Email:</label>
+          <input className="fadeIn second" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <label className="fadeIn third">Password:</label>
+          <input className="password fadeIn third" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </form>
+        <button className="fadeIn fourth center-button" type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </div>
+      <Toast ref={toast} position="top-center" />
     </div>
   );
 };
