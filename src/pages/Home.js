5015fc8e5acb3fbad7984d51eef04a8a5cef8cb4
Taksinx <zxcasd1175@hotmail.com>
2024-02-19 01:02:12 +0700

    add Home.js and Sidebar.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div className= "main" style={{ width: '100%'}}>
-      <div className="container">
+      <div className="backgroundbackground">
        <h4 className="title">Computer Engineering SUAN SUNANDHA RAJABHAT UNIVERSITY.</h4>
        <p className="info">Thai Qualifications Framework for Engineering Programs and volunteer/public relations score system: case study Computer Engineering Program <br/> มาตรฐานคุณวุฒิสำหรับหลักสูตรวิศวกรรมศาสตร์และคะแนนสาขากรณีศึกษาจากหลักสูตรวิศวกรรมคอมพิวเตอร์</p>
       </div>
